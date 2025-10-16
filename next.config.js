6a67b80a6b354060c44784b56cc9caaabd92d82e
David-ai-360-training <david.navarro@360training.com>
2025-10-15 20:52:12 -0500

    static images

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const repo = 'davidnavarro.github.io';
 
 module.exports = {
   reactStrictMode: true,
-  output: 'export',            // ahora sí válido con Next 13.4+
+  output: 'export',
   trailingSlash: true,
-  images: { unoptimized: true },
   basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-  // evita que ESLint rompa el build mientras corriges
-  eslint: { ignoreDuringBuilds: true },
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true, // if you show SVGs
+  },
 };
